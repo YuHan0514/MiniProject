af547cc2f0bfbb85dca1e75d270eea1af3a4c420
DawnLai0514 <Dawn.Lai@ibm.com>
2023-07-04 17:51:16 +0800

    fix bug

--- MiniProject/MiniProject/ClientApp/src/app/edit-stock/edit-stock.component.ts
+++ MiniProject/MiniProject/ClientApp/src/app/edit-stock/edit-stock.component.ts
@@ -37,14 +37,23 @@ export class EditStockComponent implements OnInit{
     this.router.navigateByUrl('/stock');
   }
   editStockData() {
-    let url = "https://localhost:44320/Trade//UpdateStockById";
-    let data = this.stock;
-    this.http.post(url, data)
-      .subscribe((res: any) => {
-        this.dataSvc.setPageMessage("isMessage");
-        this.dataSvc.setUrlResultMessage(res.message);
-        this.ngbModal.open(Modal01Component);
-        this.goBack();
-      })
+    if (this.stock.volume == null || this.stock.fee == null || this.stock.lendingPeriod == null || this.stock.returnDate == "") {
+      this.dataSvc.setPageMessage("isMessage");
+      this.dataSvc.setUrlResultMessage("輸入不可為空");
+      this.ngbModal.open(Modal01Component);
+    }
+    else {
+      let url = "https://localhost:44320/Trade//UpdateStockById";
+      let data = this.stock;
+      this.http.post(url, data)
+        .subscribe((res: any) => {
+          this.dataSvc.setPageMessage("isMessage");
+          this.dataSvc.setUrlResultMessage(res.message);
+          this.ngbModal.open(Modal01Component);
+          this.goBack();
+        })
+    }
   }
+
+
 }
